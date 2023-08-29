058fb0cb6dba9297ab7eac9db59b82a7701b490d
Josh Febbie <joshdfebbie@gmail.com>
2023-08-29 01:36:15 -0700

    yeup

--- api/index.js
+++ api/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require('jsonwebtoken');
 
-app.use(cors());
+const salt = bcrypt.genSaltSync(10);
+const secret = '1z2x3c4v5b6c5x4z3';
+
+app.use(cors({credentials: true, origin: ""}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://joshdfebbie:feVxRRu15zpQDe7M@cluster0.dspooud.mongodb.net/?retryWrites=true&w=majority')
@@ -12,10 +17,29 @@ mongoose.connect('mongodb+srv://joshdfebbie:feVxRRu15zpQDe7M@cluster0.dspooud.mo
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
     try {
-        const userDoc = await User.create({ username, password });
+        const userDoc = await User.create({
+          username,
+          password: bcrypt.hashSync(password, salt),
+        });
         res.json(userDoc);
-    } catch (err) {
-        res.status(400).json(err);
+    } catch (e) {
+        console.log(e);
+        res.status(400).json(e);
+    }
+})
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const userDoc = await User.findOne({ username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+    //logged in
+        jwt.sign({ username, id:userDoc._id}, secret, {}, (err, token) => {
+        if (err) throw err;
+        res.cookie("token", token).json("ok");
+     })    
+    } else {
+        res.status(400).json("Wrong Credentials");
     }
 })
 
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -4,12 +4,17 @@ export default function RegisterPage() {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     async function register(ev) {
-        ev.preventDefault()
-        await fetch("http://localhost:4000/register", {
+        ev.preventDefault();
+        const response = await fetch("http://localhost:4000/register", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password })
         })
+        if (response.status === 200) {
+            alert("registration successful")
+        } else {
+            alert("registration failed")
+        }
     }
     return (
         <form className="register" onSubmit={register}>
